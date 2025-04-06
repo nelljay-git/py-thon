8b660f440c51b1f0905827c1bd27a433b3990fef
Nell-jay <gianganwneljae@gmail.com>
2025-04-06 11:16:29 +0800

    Update simplegrade_analysis.py

--- simplegrade_analysis.py
+++ simplegrade_analysis.py
@@ -5,15 +5,15 @@ import time as time_stone
 YELLOW = "\033[93m"
 RESET = "\033[0m"
 
-name = "Mark"
-students_name = ["ESP", "TLE", "Math", "English", "Filipino", "Science", "Mapeh"]
+name = "Wneljae"
+students_subjects = ["ESP", "TLE", "Math", "English", "Filipino", "Science", "Mapeh"]
 students_id = [76, 70, 75, 74, 74, 74, 73]
-students_count = str(len(students_name))
+students_count = str(len(students_subjects))
 gpa_student = round(sum(students_id) / 7)
 
 print(f"Total students listed: {students_count}")
-for i in range(len(students_name)):
-     print(f"{students_name[i]} -> {students_id[i]}")
+for i in range(len(students_subjects)):
+     print(f"{students_subjects[i]} -> {students_id[i]}")
    
 print(f"Total GPA of {name} is: {YELLOW}{gpa_student}{RESET}")
 if gpa_student <= 75:
